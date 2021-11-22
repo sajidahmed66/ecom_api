b5261b904450e4b809660c6067fd9973aa94d473
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-11-22 17:46:08 +0600

    categoryController for get category list and create a new category

---
+++ controllers/categoryController.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+const { Category, validate } = require('../models/category');
+
+module.exports.createCategory = async (req, res) => {
+    const { error } = validate(_.pick(req.body, ['name']));
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const category = new Category(_.pick(req.body, ['name']));
+    const result = await category.save();
+    res.status(201).send({
+        message: 'Category created successfully',
+        data: _.pick(result, ['_id', 'name'])
+    });
+}
+
+module.exports.getCategories = async (req, res) => {
+    const categories = await Category.find().sort({ name: 1 });
+    return res.status(200).send({
+        data: categories
+    });
+}
+
