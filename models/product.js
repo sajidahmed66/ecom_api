1adbb121ad992542244a2bd98c0463821caa33f4
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-11-22 19:27:14 +0600

    product model created

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const joi = require('joi');
+
+module.exports.Category = model('Category', Schema({
+    name: String,
+    description: String,
+    price: Number,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    quantity: Number,
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+
+}, { timestamps: true }));
+
+module.exports.validate = (product) => {
+    const schema = {
+        name: joi.string().min(3).max(50).required(),
+        description: joi.string().min(3).max(255).required(),
+        price: joi.number().min(1).required(),
+        category: joi.string().min(3).max(50).required(),
+        quantity: joi.number().min(1).required(),
+    }
+    return joi.validate(product, schema);
+}
